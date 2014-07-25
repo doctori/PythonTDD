da32af0f7c9cf00b28258f4cb741b92a3e1e4211
doctori <lerouge.pierre@gmail.com>
2014-07-25 13:22:58 +0200

    display all element of the list, handles input

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -13,7 +13,7 @@ class NewVisitorTest(unittest.TestCase):
 	
 	def check_for_row_in_list_table(self,row_text):
 		table = self.browser.find_element_by_id('id_list_table')
-		rows = table.find_element_by_tag_name('tr')
+		rows = table.find_elements_by_tag_name('tr')
 		self.assertIn(row_text, [row.text for row in rows])
 
 	def test_can_start_a_list_and_retrieve_it_later(self):
@@ -39,7 +39,8 @@ class NewVisitorTest(unittest.TestCase):
 		#Il reste un champ de saisie pour ajouter un item
 		# L'utilisateur y ajoute "Faire un mur"
 		inputbox = self.browser.find_element_by_id('id_new_item')
-		inputbox.send_keys('Faire un mur')	
+		inputbox.send_keys('Faire un mur')
+		inputbox.send_keys(Keys.ENTER)		
 		self.check_for_row_in_list_table('1: Acheter du mortier')
 		self.check_for_row_in_list_table('2: Faire un mur')
 		self.fail('Finish the Test!')
--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -44,8 +44,28 @@ class HomePageTest(TestCase):
 
 		response = home_page(request)
 
-		expected_html = render_to_string(
-			'home.html',
-			{'new_item_text':	'A new list Item'}
-		)
-		self.assertEqual(response.content.decode(),expected_html)	
+		self.assertEqual(Item.objects.count(), 1)
+		new_item = Item.objects.first()
+		self.assertEqual(new_item.text, 'A new list Item')
+
+	def test_home_page_redirects_after_POST(self):
+		request = HttpRequest()
+		request.method = 'POST'
+		request.POST['item_text'] = 'A new list Item'
+
+		response = home_page(request)
+		self.assertEqual(response.status_code, 302)
+		self.assertEqual(response['location'], '/')
+	def test_home_page_displays_all_list_items(self):
+		Item.objects.create(text = 'item1')
+		Item.objects.create(text = 'item2')
+		request = HttpRequest()
+		response = home_page(request)
+		
+		self.assertIn('item1',response.content.decode())
+		self.assertIn('item2',response.content.decode())
+	def test_home_page_only_saves_item_when_necessary(self):
+		request = HttpRequest()
+		home_page(request)
+		self.assertEqual(Item.objects.count(), 0)
+
