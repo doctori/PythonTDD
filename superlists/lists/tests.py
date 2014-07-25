ff5a5e85963c8685f15f171793b1af658ce42d94
doctori <lerouge.pierre@gmail.com>
2014-07-25 12:34:01 +0200

    Premier model de list pour les Items et les migrations associés

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -3,7 +3,27 @@ from django.test import TestCase
 from django.http import HttpRequest
 from django.template.loader import render_to_string
 
+from lists.models import Item
+
 from lists.views import home_page
+
+class ItemModelTest(TestCase):
+	def test_saving_and_retrieving_items(self):
+		first_item = Item()
+		first_item.text = 'First List Item'
+		first_item.save()
+		
+		second_item = Item()
+		second_item.text = 'Second List Item'
+		second_item.save()
+		
+		saved_items = Item.objects.all()
+		self.assertEqual(saved_items.count(),2)
+		
+		first_saved_item = saved_items[0]
+		second_saved_item = saved_items[1]
+		self.assertEqual(first_saved_item.text,'First List Item')
+		self.assertEqual(second_saved_item.text,'Second List Item')
 
 class HomePageTest(TestCase):
 
