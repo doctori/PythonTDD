3d30c34da46280f3d965e008bdff5c7f55faa794
doctori <lerouge.pierre@gmail.com>
2014-07-31 12:26:45 +0200

    changed the database location

--- superlists/functional_tests/tests.py
+++ superlists/functional_tests/tests.py
@@ -1,3 +1,4 @@
+import sys
 from django.contrib.staticfiles.testing import StaticLiveServerCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
@@ -5,6 +6,20 @@ from selenium.webdriver.common.keys import Keys
 
 class NewVisitorTest(StaticLiveServerCase):
 	
+	@classmethod
+	def setUpClass(cls):
+		for arg in sys.argv:
+			if 'liveserver' in arg:
+				cls.server_url = 'http://' + arg.split('=')[1]
+				return
+		super().setUpClass()
+		cls.server_url = cls.live_server_url
+
+	@classmethod
+	def tearDownClass(cls):
+		if cls.server_url == cls.live_server_url:
+			super().tearDownClass()
+
 	def setUp(self):
 		self.browser = webdriver.Firefox()
 		self.browser.implicitly_wait(3)	
@@ -16,11 +31,18 @@ class NewVisitorTest(StaticLiveServerCase):
 		table = self.browser.find_element_by_id('id_list_table')
 		rows = table.find_elements_by_tag_name('tr')
 		self.assertIn(row_text, [row.text for row in rows])
-
+	def test_layout_and_styling(self):
+		self.browser.get(self.server_url)
+		self.browser.set_window_size(1024,768)
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		self.assertAlmostEqual(
+			inputbox.location['x'] + inputbox.size['width']/2,
+			512,
+			delta=5
+		)
 	def test_can_start_a_list_and_retrieve_it_later(self):
 		#acces a la page de garde
-		self.browser.set_window_size(1024,768)
-		self.browser.get(self.live_server_url)
+		self.browser.get(self.server_url)
 		# le titre de la page est 'To-Do' et le titre sur la page
 		self.assertIn('To-Do', self.browser.title)
 		header_text = self.browser.find_element_by_tag_name('h1').text
@@ -28,11 +50,6 @@ class NewVisitorTest(StaticLiveServerCase):
 		#le champ de saisi est bien centré
 		# L'utilisateur est invité a saisir une action 'To-Do'
 		inputbox = self.browser.find_element_by_id('id_new_item')
-		self.assertAlmostEqual(
-			inputbox.location['x'] + inputbox.size['width']/2,
-			512,
-			delta=5
-		)
 		self.assertEqual(
 			inputbox.get_attribute('placeholder'),
 			'Enter a to-do item'
@@ -49,12 +66,6 @@ class NewVisitorTest(StaticLiveServerCase):
 		#Il reste un champ de saisie pour ajouter un item
 		# L'utilisateur y ajoute "Faire un mur"
 		inputbox = self.browser.find_element_by_id('id_new_item')
-		# L'input box doit aussi etre centrée
-		self.assertAlmostEqual(
-			inputbox.location['x'] + inputbox.size['width']/2,
-			512,
-			delta=5
-		)
 		inputbox.send_keys('Faire un mur')
 		inputbox.send_keys(Keys.ENTER)		
 		self.check_for_row_in_list_table('1: Acheter du mortier')
@@ -65,7 +76,7 @@ class NewVisitorTest(StaticLiveServerCase):
 		self.browser = webdriver.Firefox()
 		self.browser.implicitly_wait(3)	
 		#user2 Visite la page d'accueil
-		self.browser.get(self.live_server_url)
+		self.browser.get(self.server_url)
 		# pas de traces des enregistrement du user1 
 		page_text = self.browser.find_element_by_tag_name('body').text
 		self.assertNotIn('Acheter du mortier',page_text)
