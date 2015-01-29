dbc67a242dc52fb79e84fffe0c3e0d46d121a9db
doctori <lerouge.pierre@gmail.com>
2015-01-29 22:19:25 +0100

    added first javascript oriented FT (and migration dependency correction

--- superlists/functional_tests/test_list_item_validation.py
+++ superlists/functional_tests/test_list_item_validation.py
@@ -4,6 +4,18 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 class ItemValidationTest(FunctionalTest):
+	def test_error_messages_are_cleared_on_input(self):
+		#Creation d'une liste qui genere une erreur :
+		self.browser.get(self.server_url)
+		self.get_item_input_box().send_keys('\n')
+		error = self.browser.find_element_by_css_selector('.has-error')
+		self.assertTrue(error.is_displayed())
+		
+		#l'utilisateur corrige l'erreur
+		self.get_item_input_box().send_keys('yeah sorry')
+		error = self.browser.find_element_by_css_selector('.has-error')
+		self.assertFalse(error.is_displayed())
+		
 	def test_cannot_add_empty_list_item(self):
 		#L'utilisateur arrive sur la page principale et essaye d'ajouter un item vide
 		# il appuie sur entrÉ
---
+++ superlists/lists/migrations/0006_list_item_uniqueness.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('lists', '0005_Item_foreign_key_for_lists'),
+    ]
+
+    operations = [
+        migrations.AlterModelOptions(
+            name='item',
+            options={'ordering': ('id',)},
+        ),
+        migrations.AlterUniqueTogether(
+            name='item',
+            unique_together=set([('list', 'text')]),
+        ),
+    ]
