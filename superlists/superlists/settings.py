1b080e3a8e11f3cbb306e495618052079effbbda
doctori <lerouge.pierre@gmail.com>
2015-02-01 20:46:57 +0100

    authentication from backend starting to be tested and build

--- superlists/superlists/settings.py
+++ superlists/superlists/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
 	'lists',
+	'accounts',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -83,3 +84,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.abspath(os.path.join(BASE_DIR, '../static'))
+STATICFILES_DIRS = (
+	os.path.join(BASE_DIR, 'superlists','static'),
+)
