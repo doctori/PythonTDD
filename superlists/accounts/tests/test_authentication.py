1ba51ee91e2ab84ea7fd065299912dd0700126e5
doctori <lerouge.pierre@gmail.com>
2015-02-01 21:28:25 +0100

    test for non authenticated user if bad response'

--- superlists/accounts/tests/test_authentication.py
+++ superlists/accounts/tests/test_authentication.py
@@ -4,9 +4,10 @@ from mock import patch
 from accounts.authentication import (
 	PERSONA_VERIFY_URL, DOMAIN, PersonaAuthenticationBackend
 )
+
+@patch('accounts.authentication.requests.post')
 class testAuthentication(TestCase):
 
-	@patch('accounts.authentication.requests.post')
 	def test_sends_assertion_to_mozilla_with_domain(self, mock_post):
 		backend = PersonaAuthenticationBackend()
 		backend.authenticate('assert this')
@@ -14,3 +15,9 @@ class testAuthentication(TestCase):
 			PERSONA_VERIFY_URL,
 			data={'assertion':'assert this','audience':DOMAIN}
 		)
+
+	def test_returns_none_if_response_error(self, mock_post):
+		mock_post.return_value.ok = False
+		backend = PersonaAuthenticationBackend()
+		user = backend.authenticate('assert this')
+		self.assertIsNone(user)
